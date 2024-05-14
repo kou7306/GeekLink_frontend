94e3866fb859a85c10cad7bc9730e5c8a9da46e8
imaikosuke <imaikosuke2@gmail.com>
2024-05-15 00:41:53 +0900

    技術の選択肢を増幅

--- src/app/profile-initialization/options.ts
+++ src/app/profile-initialization/options.ts
@@ -2,7 +2,7 @@
 
 export const ages = Array.from({ length: 66 }, (_, i) => i + 15);
 
-export const residences = [
+export const places = [
   "北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県", "茨城県", "栃木県", "群馬県",
   "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県",
   "岐阜県", "静岡県", "愛知県", "三重県", "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県",
@@ -11,10 +11,15 @@ export const residences = [
 ];
 
 export const technologies = [
-  "Java", "JavaScript", "Python", "Ruby", "PHP", "React", "Angular", "Vue.js"
+  "JavaScript", "TypeScript", "React", "Vue", "Angular", "Node", "Next.js", "Nuxt.js",
+  "jQuery", "Sass", "TailwindCSS", "Bootstrap", "PHP", "Laravel", 
+  "C", "C++", "C#", "Go", "Rust", "Java", "Spring-Boot", "Unity", "Kotlin", "Swift", "Flutter", "Dart", "Objective-C", "Ruby",
+  "Ruby on Rails", "Python", "Django", "Flask", "FastAPI", "scala", "Perl", "R",
+  "MySQL", "PostgreSQL", "SQLite", "MongoDB", "Firebase", "Supabase", "DynamoDB", "Redis",
+  "GitHub", "Docker", "AWS", "GCP", "Azure", "Heroku", "Vercel", "Netlify", 
 ];
 
-export const jobTitles = [
+export const occupations = [
   "高校生１年生", "高校生２年生", "高校生３年生", "高専１年生", "高専２年生", "高専３年生",
   "高専４年生", "高専５年生", "大学生１年生", "大学生２年生", "大学生３年生", "大学生４年生",
   "専門学校１年生", "専門学校２年生", "IT企業エンジニア", "フリーランスエンジニア", "その他"
