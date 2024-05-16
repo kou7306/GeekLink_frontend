7b2fe9840e8f787ec7b0ced5d355cce9e223db02
kou7306 <kouta.1206.y@icloud.com>
2024-05-17 01:15:06 +0900

    socket

--- src/app/chats/page.tsx
+++ src/app/chats/page.tsx
@@ -1,12 +1,54 @@
-import React from "react";
-import { Chats } from "./chats";
+"use client";
+import { useEffect, useRef, useState } from "react";
 
-const page = () => {
+const Page = () => {
+  const socketRef = useRef<WebSocket>();
+  const [isConnected, setIsConnected] = useState(false);
+  const [formMessage, setFormMessage] = useState("");
+  const [sentMessage, setSentMessage] = useState("");
+
+  const sendData = (event: any) => {
+    event.preventDefault();
+    setFormMessage(event.target[0].value);
+    socketRef.current?.send(event.target[0].value);
+  };
+
+  useEffect(() => {
+    socketRef.current = new WebSocket("ws://localhost:8080/ws");
+    socketRef.current.onopen = function () {
+      setIsConnected(true);
+      console.log("Connected");
+    };
+
+    socketRef.current.onclose = function () {
+      console.log("closed");
+      setIsConnected(false);
+    };
+
+    // server 側から送られてきたデータを受け取る
+    socketRef.current.onmessage = function (event) {
+      setSentMessage(event.data);
+    };
+
+    return () => {
+      if (socketRef.current == null) {
+        return;
+      }
+      socketRef.current.close();
+    };
+  }, []);
+
   return (
-    <div>
-      <Chats />
-    </div>
+    <>
+      <h1>WebSocket is connected : {`${isConnected}`}</h1>
+      <form onSubmit={sendData}>
+        <input type="text" name="socketData" />
+        <button type="submit">Server に送信</button>
+      </form>
+      {/* <h3>form message: {formMessage}</h3> */}
+      <h3>sent message: {sentMessage}</h3>
+    </>
   );
 };
 
-export default page;
+export default Page;
