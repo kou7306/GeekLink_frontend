0826f6468d556121fc490e70bf50960add77981f
na2kera <nat3mitaro@gmail.com>
2024-05-18 23:58:58 +0900

    ヘッダーを作成

---
+++ src/app/Header.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { FaHeart, FaSearch, FaShoppingCart, FaUser } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header className="bg-white shadow-md">
+      <div className="container mx-auto flex justify-between items-center py-4 px-6">
+        <div className="text-2xl font-bold">
+          <a href="/" className="flex items-center">
+            <span className="text-accent">GeekLink</span>
+          </a>
+        </div>
+        <nav className="flex space-x-6">
+          <a href="/login" className="text-black hover:text-accent">
+            ログイン
+          </a>
+          <a href="/sign-up" className="text-black hover:text-accent">
+            サインアップ
+          </a>
+          <a href="/message" className="text-black hover:text-accent">
+            メッセージ
+          </a>
+          <a href="/random-match" className="text-black hover:text-accent">
+            ランダムマッチ
+          </a>
+        </nav>
+        <div className="flex space-x-1">
+          <a
+            href="/my-page"
+            className="text-black hover:text-gray-700 flex items-center hover:text-accent"
+          >
+            <FaUser className="mr-1 hover:text-accent" />
+            マイページ
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
