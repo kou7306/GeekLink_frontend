397c31b6e172c9b4de07b65b78defe7fd78dbb06
imaikosuke <imaikosuke2@gmail.com>
2024-05-19 11:28:55 +0900

    ユーザー情報を更新

--- src/components/profile/DesireOccupationSelect.tsx
+++ src/components/profile/DesireOccupationSelect.tsx
@@ -3,21 +3,29 @@ import React from "react";
 import { desiredOccupationOptions } from "./options";
 
 interface DesireOccupationSelectProps {
-  desireOccupation?: string;
+  desire_occupation?: string;
   onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
 }
 
-const DesireOccupationSelect: React.FC<DesireOccupationSelectProps> = ({ desireOccupation, onChange }) => {
+const DesireOccupationSelect: React.FC<DesireOccupationSelectProps> = ({
+  desire_occupation,
+  onChange,
+}) => {
   return (
     <div>
-      <label htmlFor="desireOccupation" className="block text-sm font-medium text-gray-700">
+      <label htmlFor="desire_occupation" className="block text-sm font-medium text-gray-700">
         希望職種
       </label>
-      <select name="desireOccupation" value={desireOccupation} onChange={onChange} className="mt-1 block w-full">
+      <select
+        name="desire_occupation"
+        value={desire_occupation}
+        onChange={onChange}
+        className="mt-1 block w-full"
+      >
         <option value="">選択してください</option>
-        {desiredOccupationOptions.map((desireOccupation) => (
-          <option key={desireOccupation} value={desireOccupation}>
-            {desireOccupation}
+        {desiredOccupationOptions.map((desire_occupation) => (
+          <option key={desire_occupation} value={desire_occupation}>
+            {desire_occupation}
           </option>
         ))}
       </select>
