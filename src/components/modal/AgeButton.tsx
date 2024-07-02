2fbcc21a9f79d0173c33be2921e27bb4ecb552e7
na2kera <nat3mitaro@gmail.com>
2024-07-02 14:29:27 +0900

    mapを削除

--- src/components/modal/AgeButton.tsx
+++ src/components/modal/AgeButton.tsx
@@ -5,14 +5,12 @@ const AgeButton = () => {
   const ageGroups = ["~14歳", "15~17歳", "18~22歳", "23~29歳", "30歳~"];
   return (
     <>
-      {ageGroups.map((ageGroup) => (
-        <Button sx={boxStyle}>{ageGroup}</Button>
-      ))}
+      <Button sx={BoxStyle}>ageGroup</Button>
     </>
   );
 };
 
-const boxStyle = {
+const BoxStyle = {
   border: "1px solid #22d3ee",
   borderRadius: "8px",
   padding: "8px 16px",
