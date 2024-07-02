d2f0f461a4d6a7edf9033b4eae2d673cecfa3c7f
na2kera <nat3mitaro@gmail.com>
2024-07-02 15:26:44 +0900

    卒業年度コンポーネントを追加

---
+++ src/components/modal/GraduateButton.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { graduateOptions } from "../profile/options";
+import { Button } from "@mui/material";
+
+const GraduateButton = () => {
+  return (
+    <>
+      <>
+        {graduateOptions.map((graduateOption) => (
+          <Button sx={BoxStyle}>{graduateOption}</Button>
+        ))}
+      </>
+    </>
+  );
+};
+
+const BoxStyle = {
+  border: "1px solid #22d3ee",
+  borderRadius: "8px",
+  padding: "8px 16px",
+  display: "inline-block",
+  color: "#22d3ee",
+  textAlign: "center",
+  marginX: 1,
+  marginBottom: 1,
+};
+
+export default GraduateButton;
