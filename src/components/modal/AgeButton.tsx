256323c9afaddf1201aa632dcf70073fe6179ec6
na2kera <nat3mitaro@gmail.com>
2024-07-02 14:22:08 +0900

    ボタン追加

--- src/components/modal/AgeButton.tsx
+++ src/components/modal/AgeButton.tsx
@@ -3,7 +3,13 @@ import React from "react";
 
 const AgeButton = () => {
   const ageGroups = ["~14歳", "15~17歳", "18~22歳", "23~29歳", "30歳~"];
-  return <></>;
+  return (
+    <>
+      {ageGroups.map((ageGroup) => (
+        <Button sx={BoxStyle}>{ageGroup}</Button>
+      ))}
+    </>
+  );
 };
 
 const BoxStyle = {
