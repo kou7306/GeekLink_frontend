460b4a506ad1d9f11f892c2ba356d94a841e603e
kappy0713 <s22253@g.ashikaga.ac.jp>
2024-09-29 01:30:13 +0900

    feat: GitHub OAuthフロントサンプル

---
+++ src/app/github/redirect/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useRouter, useSearchParams } from "next/navigation";
+import { getUuidFromCookie } from "@/actions/users";
+
+export default function GithubCallback() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const [uuid, setUuid] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    const fetchUuid = async () => {
+      const fetchedUuid = await getUuidFromCookie();
+      setUuid(fetchedUuid);
+    };
+    fetchUuid();
+  }, []);
+
+  useEffect(() => {
+    const code = searchParams.get("code");
+    const api_url = process.env.NEXT_PUBLIC_API_URL;
+
+    if (code && uuid) {
+      console.log("GitHub Auth Code:", code);
+      console.log("UUID:", uuid);
+      axios
+        .post(`${api_url}/github/callback`, { uuid, code })
+        .then((response) => {
+          console.log("GitHub Token saved:", response.data);
+          router.push("/");
+        })
+        .catch((error) => {
+          console.error("Error saving GitHub token:", error);
+        });
+    }
+  }, [searchParams, uuid]);
+
+  return <div>Processing GitHub Authentication...</div>;
+}
