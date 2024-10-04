882023cb0fe361d2b4757389a5b6f49c37ab27cd
na2kera <nat3mitaro@gmail.com>
2024-10-05 01:30:43 +0900

    レポジトリごとのグラフを表示

---
+++ src/components/profile/RepositoryGraph.tsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+type Props = {
+  repository: Repository;
+};
+
+const RepositoryGraph = ({ repository }: Props) => {
+  const repositoryData = {
+    labels: [""],
+    datasets: Object.entries(repository.languages).map(([key, value]) => ({
+      // 言語
+      label: value.name,
+      // 割合
+      data: [value.percentage],
+      backgroundColor: (() => {
+        switch (value.name) {
+          case "TypeScript":
+            return "#3178C6";
+          case "JavaScript":
+            return "#F7DF1E";
+          case "Python":
+            return "#3776AB";
+          case "Java":
+            return "#007396";
+          case "C++":
+            return "#00599C";
+          case "Ruby":
+            return "#CC342D";
+          case "Go":
+            return "#00ADD8";
+          case "Rust":
+            return "#DEA584";
+          default:
+            return "#808080"; // Default color for other languages
+        }
+      })(),
+    })),
+  };
+  const options = {
+    indexAxis: "y" as const,
+    scales: {
+      x: {
+        stacked: true,
+        max: 100,
+        ticks: {
+          callback: function (value: string | number) {
+            return value + "%";
+          },
+        },
+        grid: {
+          display: false,
+        },
+      },
+      y: {
+        stacked: true,
+        grid: {
+          display: false,
+        },
+      },
+    },
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: `${repository.name}`,
+      },
+    },
+    layout: {
+      padding: {
+        top: 30,
+        bottom: 30,
+      },
+    },
+  };
+  return <Bar data={repositoryData} options={options} />;
+};
+
+export default RepositoryGraph;
--- src/components/profile/RepositoryList.tsx
+++ src/components/profile/RepositoryList.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Bar } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   Tooltip,
@@ -10,6 +9,7 @@ import {
   Title,
 } from "chart.js";
 import { getUuidFromCookie } from "@/actions/users";
+import RepositoryGraph from "./RepositoryGraph";
 
 ChartJS.register(
   CategoryScale,
@@ -44,64 +44,13 @@ const RepositoryList = () => {
     fetchRepositories();
   }, []);
 
-  const data = {
-    labels: [""],
-    datasets: [
-      {
-        label: "bad",
-        data: [99.35801996967669],
-        backgroundColor: "rgba(244, 143, 177, 0.6)",
-      },
-      {
-        label: "better",
-        data: [0.6419800303233121],
-        backgroundColor: "rgba(255, 235, 59, 0.6)",
-      },
-    ],
-  };
-
-  const options = {
-    indexAxis: "y" as const,
-    scales: {
-      x: {
-        stacked: true,
-        max: 100,
-        ticks: {
-          callback: function (value: string | number) {
-            return value + "%";
-          },
-        },
-        grid: {
-          display: false,
-        },
-      },
-      y: {
-        stacked: true,
-        grid: {
-          display: false,
-        },
-      },
-    },
-    responsive: true,
-    plugins: {
-      legend: {
-        position: "top" as const,
-      },
-      title: {
-        display: true,
-        text: "Repository Distribution",
-      },
-    },
-    layout: {
-      padding: {
-        top: 30,
-        bottom: 30,
-      },
-    },
-  };
   return (
     <div style={{ width: "80%", height: "200px", margin: "auto" }}>
-      <Bar data={data} options={options} />
+      {/* 一つあたりのグラフ */}
+      {repositories.length > 0 &&
+        repositories.map((repository, index) => (
+          <RepositoryGraph key={index} repository={repository} />
+        ))}
     </div>
   );
 };
