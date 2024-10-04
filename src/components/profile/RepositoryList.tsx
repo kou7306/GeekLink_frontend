6a086670a0af356eab4db4959efe73131d7770da
na2kera <nat3mitaro@gmail.com>
2024-10-05 01:09:43 +0900

    グラフのスタイルを変更

--- src/components/profile/RepositoryList.tsx
+++ src/components/profile/RepositoryList.tsx
@@ -1,29 +1,104 @@
-import React from "react";
-import { Doughnut } from "react-chartjs-2";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import React, { useEffect, useState } from "react";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+} from "chart.js";
+import { getUuidFromCookie } from "@/actions/users";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const RepositoryList = () => {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  //個人のリポジトリを上から十件取得
+  useEffect(() => {
+    const fetchRepositories = async () => {
+      const uuid = await getUuidFromCookie();
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/github/repo`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ uuid }),
+        }
+      );
+      const data = await res.json();
+      setRepositories(data);
+      console.log(data);
+    };
+    fetchRepositories();
+  }, []);
+
+  // const data = {
+  //   labels: ["Red", "Blue", "Yellow"],
+  //   datasets: [
+  //     {
+  //       label: "My First Dataset",
+  //       data: [300, 50, 100],
+  //       backgroundColor: [
+  //         "rgb(255, 99, 132)",
+  //         "rgb(54, 162, 235)",
+  //         "rgb(255, 205, 86)",
+  //       ],
+  //       hoverOffset: 4,
+  //     },
+  //   ],
+  // };
+
   const data = {
-    labels: ["Red", "Blue", "Yellow"],
+    labels: [""],
     datasets: [
       {
-        label: "My First Dataset",
-        data: [300, 50, 100],
-        backgroundColor: [
-          "rgb(255, 99, 132)",
-          "rgb(54, 162, 235)",
-          "rgb(255, 205, 86)",
-        ],
-        hoverOffset: 4,
+        label: "bad",
+        data: [99.35801996967669],
+        backgroundColor: "rgba(244, 143, 177, 0.6)",
+      },
+      {
+        label: "better",
+        data: [0.6419800303233121],
+        backgroundColor: "rgba(255, 235, 59, 0.6)",
       },
     ],
   };
 
   const options = {
+    indexAxis: "y" as const,
+    scales: {
+      x: {
+        stacked: true,
+        max: 100,
+        ticks: {
+          callback: function (value: string | number) {
+            return value + "%";
+          },
+        },
+        grid: {
+          display: false,
+        },
+      },
+      y: {
+        stacked: true,
+        grid: {
+          display: false,
+        },
+      },
+    },
     responsive: true,
-    maintainAspectRatio: false,
     plugins: {
       legend: {
         position: "top" as const,
@@ -31,17 +106,19 @@ const RepositoryList = () => {
       title: {
         display: true,
         text: "Repository Distribution",
-        font: {
-          size: 18,
-        },
       },
     },
-    cutout: "50%",
+    layout: {
+      padding: {
+        top: 30,
+        bottom: 30,
+      },
+    },
   };
-
   return (
-    <div style={{ width: "80%", height: "400px", margin: "auto" }}>
-      <Doughnut data={data} options={options} />
+    <div style={{ width: "80%", height: "200px", margin: "auto" }}>
+      {/* <Doughnut data={data} options={options} /> */}
+      <Bar data={data} options={options} />
     </div>
   );
 };
