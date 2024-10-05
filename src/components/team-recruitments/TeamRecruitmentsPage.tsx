61e4b68db622aa71da1a3f6be515c2f89375e4d4
imaikosuke <imaikosuke2@gmail.com>
2024-10-05 20:02:25 +0900

    feature: イベントを新規で作成するページとAPI統合

--- src/components/team-recruitments/TeamRecruitmentsPage.tsx
+++ src/components/team-recruitments/TeamRecruitmentsPage.tsx
@@ -5,6 +5,8 @@ import TeamRecruitmentTabs from "./TeamRecruitmentsTabs";
 import TeamRecruitmentPanels from "./TeamRecruitmentsPanels";
 import Options from "./Options";
 import { Event } from "@/types/event";
+import Link from "next/link";
+import { FaPlus } from "react-icons/fa";
 
 interface TeamRecruitmentPageProps {
   hackathonEvents: Event[];
@@ -21,6 +23,28 @@ const TeamRecruitmentPage = ({ hackathonEvents, eventEvents }: TeamRecruitmentPa
       <Options />
       {/* チーム募集 */}
       <TeamRecruitmentPanels value={value} hackathonEvents={hackathonEvents} eventEvents={eventEvents} />
+      <Link href="/team-recruitments/create">
+        <button
+          style={{
+            position: "fixed",
+            right: "20px",
+            bottom: "20px",
+            width: "60px",
+            height: "60px",
+            borderRadius: "50%",
+            backgroundColor: "#007bff",
+            color: "white",
+            border: "none",
+            fontSize: "24px",
+            cursor: "pointer",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <FaPlus />
+        </button>
+      </Link>
     </>
   );
 };
