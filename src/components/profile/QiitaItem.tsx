47b87d56fd9ca177b91b61c5a2cf45efb1800e4a
na2kera <nat3mitaro@gmail.com>
2024-10-09 15:26:59 +0900

    日付をフォーマット

--- src/components/profile/QiitaItem.tsx
+++ src/components/profile/QiitaItem.tsx
@@ -1,4 +1,4 @@
-import { Box, Paper, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
 import { QiitaArticle } from "../../../types/qiitaArticle";
 import { formatDate } from "@/utils/formatDate";
@@ -12,14 +12,15 @@ const QiitaItem = ({ item }: Props) => {
   return (
     <Link href={item.url} target="_blank" rel="noopener noreferrer">
       <Box
-        height={"200px"}
         border={"1px solid #e0e0e0"}
         borderRadius={"20px"}
-        padding={2}
+        pb={3}
+        pt={2}
+        px={2}
         marginBottom={2}
       >
         <Typography variant="body2" color="text.secondary" gutterBottom>
-          {formatDate(item.createdAt)} {/* 何月何日の形にフォーマット */}
+          {formatDate(item.date)} {/* 何月何日の形にフォーマット */}
         </Typography>
         <Box
           display={"flex"}
