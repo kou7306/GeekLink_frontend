384730e3ec93a5a5d47675f51a8757a7c1f84c94
na2kera <nat3mitaro@gmail.com>
2024-10-11 23:05:41 +0900

    MUIのカラー

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,18 +1,26 @@
 import { createTheme } from "@mui/material";
 
-//MUIのテーマをカスタマイズ
+// MUIのテーマをカスタマイズ
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#22d3ee",
-      contrastText: "#111827",
+      main: "#2ecee6", // メインカラー
+      contrastText: "#ffffff",
     },
     secondary: {
-      main: "#25276D",
-      contrastText: "white",
+      main: "#2c64c5", // メインの補助カラー
+      contrastText: "#ffffff",
+    },
+    background: {
+      default: "#2b2a36", // 背景
+      paper: "#000000", // コンテンツの背景
     },
     text: {
-      primary: "#111827",
+      primary: "#ffffff", // テキスト
+      secondary: "#b5b5ba", // 枠線
+    },
+    error: {
+      main: "#ee223c", // メインとは反対の色
     },
   },
 });
