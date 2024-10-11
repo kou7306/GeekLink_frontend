2e01b9da455fb3f6f28a702fef417bd3df7f6d04
amatuzi <80336703+kou7306@users.noreply.github.com>
2024-10-11 21:55:56 +0900

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,13 +10,13 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        primary: "#2ecee6", //メインカラー
-        secondary: "#2c64c5", //メインの補助カラー
-        base: "#2b2a36", //背景
-        sub_base: "#000000", //コンテンツの背景
-        border: "#b5b5ba", //枠線
-        accent: "#ee223c", //メインとは反対の色
-        text: "#ffffff", //テキスト
+        primary: "#2cb1c5",
+        secondary: "#f3f4f6",
+        base: "#f0f2f5",
+        sub_base: "#ffffff",
+        border: "#e5e7eb",
+        accent: "#ee223c",
+        text: "#111827",
       },
     },
   },
