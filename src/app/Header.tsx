816f2dd87ed39d47e9efbcf7556f22adb6f72d0a
na2kera <nat3mitaro@gmail.com>
2024-10-11 20:55:45 +0900

    ヘッダーが今いるところに色がつくように変更

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,34 +1,62 @@
+"use client";
 import SignOutButton from "@/components/auth/SignOutButton";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { FaUser } from "react-icons/fa";
 
 const Header = () => {
+  const pathname = usePathname();
+
+  const isActive = (path: string) => pathname === path;
+
   return (
     <header className="bg-white shadow-md fixed top-0 w-full z-50">
       <div className="container mx-auto flex justify-between items-center py-4 px-6">
         <div className="text-2xl font-bold">
           <a href="/" className="flex items-center">
-            <span className="text-accent">GeekLink</span>
+            <span className="text-primary">GeekLink</span>
           </a>
         </div>
         <nav className="flex space-x-6">
-          <a href="/conversation" className="text-black hover:text-accent">
+          <a
+            href="/conversation"
+            className={`text-black hover:text-primary ${
+              isActive("/conversation") ? "text-primary" : ""
+            }`}
+          >
             メッセージ
           </a>
-          <a href="/group-list" className="text-black hover:text-accent">
+          <a
+            href="/group-list"
+            className={`text-black hover:text-primary ${
+              isActive("/group-list") ? "text-primary" : ""
+            }`}
+          >
             グループ
           </a>
-          <a href="/suggest-users" className="text-black hover:text-accent">
+          <a
+            href="/suggest-users"
+            className={`text-black hover:text-primary ${
+              isActive("/suggest-users") ? "text-primary" : ""
+            }`}
+          >
             おすすめユーザー
           </a>
-          <a href="/like" className="text-black hover:text-accent">
+          <a
+            href="/like"
+            className={`text-black hover:text-primary ${
+              isActive("/like") ? "text-primary" : ""
+            }`}
+          >
             いいね
           </a>
         </nav>
-        <div className="flex space-x-1 hover:text-accent">
+        <div className="flex space-x-1 hover:text-primary">
           <a
             href="/my-page"
-            className="text-black hover:text-accent flex items-center bg-secondary text-black py-2 px-4 rounded"
+            className={`text-black hover:text-primary flex items-center bg-secondary text-black py-2 px-4 rounded ${
+              isActive("/my-page") ? "text-primary" : ""
+            }`}
           >
             <FaUser className="mr-2 " />
             マイページ
