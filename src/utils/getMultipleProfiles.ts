63b14a0346c3c14c90bdaf1c9f549485623b3d94
na2kera <nat3mitaro@gmail.com>
2024-10-21 17:12:01 +0900

    キャッシュを設定

--- src/utils/getMultipleProfiles.ts
+++ src/utils/getMultipleProfiles.ts
@@ -6,7 +6,9 @@ export async function getMultipleProfiles(uuids: string[]): Promise<User[]> {
 
   try {
     const requests = uuids.map((uuid) =>
-      fetch(`${apiUrl}/profile/get-profile/${uuid}`).then((response) => {
+      fetch(`${apiUrl}/profile/get-profile/${uuid}`, {
+        next: { revalidate: 3600 }, // 1時間ごとに再検証
+      }).then((response) => {
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
