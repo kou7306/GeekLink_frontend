286302a9becff33715df33292d7f1e7a2d7a98c3
kou7306 <kouta.1206.y@icloud.com>
2024-11-07 21:22:36 +0900

    popupのUI

--- src/components/BonusPopup.tsx
+++ src/components/BonusPopup.tsx
@@ -1,4 +1,3 @@
-// BonusPopup.tsx
 import React from "react";
 
 interface BonusPopupProps {
@@ -21,18 +20,30 @@ const BonusPopup: React.FC<BonusPopupProps> = ({
   onClose,
 }) => {
   return (
-    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black border border-gray-300 rounded-lg shadow-lg p-6 z-50">
-      <h2 className="text-xl font-bold">おめでとうございます！</h2>
-      <p className="mt-2">ログインボーナスを獲得しました！</p>
-      <p className="mt-2">ライフ: {lifeAdded}</p>
-      <p className="mt-2">コイン: {coinsAdded}</p>
-      {/* <p className="mt-2">連続ログイン日数: {userData.login_streak}</p> */}
-      <button
-        className="mt-4 bg-blue-500 text-white rounded-md px-4 py-2 hover:bg-blue-600"
-        onClick={onClose}
-      >
-        閉じる
-      </button>
+    <div className="fixed inset-0 bg-sub_base bg-opacity-50 flex items-center justify-center z-50">
+      <div className="border border-primary rounded-lg shadow-lg p-6 max-w-xs w-full text-center relative">
+        <h2 className="text-2xl font-bold text-yellow-400 mb-4 whitespace-nowrap">
+          おめでとうございます！
+        </h2>
+        <p className="text-mg mb-2 whitespace-nowrap">
+          ログインボーナスを獲得しました
+        </p>
+
+        {lifeAdded !== undefined && (
+          <p className="text-lg mt-2 whitespace-nowrap">ライフ: {lifeAdded}</p>
+        )}
+
+        {coinsAdded !== undefined && (
+          <p className="text-lg mt-2 whitespace-nowrap">コイン: {coinsAdded}</p>
+        )}
+
+        <button
+          className="mt-4 bg-primary text-text rounded-md px-4 py-2 hover:bg-hover_blue whitespace-nowrap"
+          onClick={onClose}
+        >
+          閉じる
+        </button>
+      </div>
     </div>
   );
 };
