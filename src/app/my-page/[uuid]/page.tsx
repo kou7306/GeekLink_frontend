dfeccc1621fcea8d504f199ac575cb657d6911b4
na2kera <nat3mitaro@gmail.com>
2024-12-17 17:15:54 +0900

    不要な部分を削除

--- src/app/my-page/[uuid]/page.tsx
+++ src/app/my-page/[uuid]/page.tsx
@@ -1,7 +1,4 @@
-// "use client";
-
 import ProfileCard from "@/components/profile/ProfileCard";
-
 import Loading from "@/components/core/Loading";
 import { getUuidFromCookie } from "@/actions/users";
 import { redirect } from "next/navigation";
@@ -20,16 +17,7 @@ async function fetchUser(uuid: string) {
   return response.json();
 }
 
-const UserProfile: React.FC = async ({
-  params,
-}: {
-  params: { uuid: string };
-}) => {
-  // const [user, setUser] = useState<User | null>(null);
-  // const params = useParams();
-  // const uuid = params.uuid;
-  // const router = useRouter();
-
+const UserProfile = async ({ params }: { params: { uuid: string } }) => {
   const uuid = params.uuid;
 
   const cookieUuid = await getUuidFromCookie();
@@ -51,20 +39,3 @@ const UserProfile: React.FC = async ({
 };
 
 export default UserProfile;
-
-//       if (uuid === cookieUuid) {
-//         router.push("/my-page");
-//       } else if (uuid) {
-//         axios
-//           .get(`${process.env.NEXT_PUBLIC_API_URL}/profile/get-profile/${uuid}`)
-//           .then((response) => {
-//             setUser(response.data);
-//           })
-//           .catch((error) => {
-//             console.error("Error fetching user data:", error);
-//           });
-//       }
-//     };
-
-//     checkAndRedirect();
-//   }, [uuid, router]);
