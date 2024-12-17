6cfbee0b483e129fb3fa3bc60bd85316cd457bf8
na2kera <nat3mitaro@gmail.com>
2024-12-17 17:13:08 +0900

    ssr

--- src/app/my-page/[uuid]/page.tsx
+++ src/app/my-page/[uuid]/page.tsx
@@ -1,37 +1,43 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { useParams, useRouter } from "next/navigation";
+// "use client";
+
 import ProfileCard from "@/components/profile/ProfileCard";
-import { User } from "@/components/profile/options";
+
 import Loading from "@/components/core/Loading";
 import { getUuidFromCookie } from "@/actions/users";
+import { redirect } from "next/navigation";
 
-const UserProfile: React.FC = () => {
-  const [user, setUser] = useState<User | null>(null);
-  const params = useParams();
+async function fetchUser(uuid: string) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/profile/get-profile/${uuid}`,
+    {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return response.json();
+}
+
+const UserProfile: React.FC = async ({
+  params,
+}: {
+  params: { uuid: string };
+}) => {
+  // const [user, setUser] = useState<User | null>(null);
+  // const params = useParams();
+  // const uuid = params.uuid;
+  // const router = useRouter();
+
   const uuid = params.uuid;
-  const router = useRouter();
 
-  useEffect(() => {
-    const checkAndRedirect = async () => {
-      const cookieUuid = await getUuidFromCookie();
-      if (uuid === cookieUuid) {
-        router.push("/my-page");
-      } else if (uuid) {
-        axios
-          .get(`${process.env.NEXT_PUBLIC_API_URL}/profile/get-profile/${uuid}`)
-          .then((response) => {
-            setUser(response.data);
-          })
-          .catch((error) => {
-            console.error("Error fetching user data:", error);
-          });
-      }
-    };
+  const cookieUuid = await getUuidFromCookie();
+  if (uuid === cookieUuid) {
+    return redirect("/my-page");
+  }
 
-    checkAndRedirect();
-  }, [uuid, router]);
+  const user = await fetchUser(uuid);
 
   if (!user) {
     return <Loading />;
@@ -45,3 +51,20 @@ const UserProfile: React.FC = () => {
 };
 
 export default UserProfile;
+
+//       if (uuid === cookieUuid) {
+//         router.push("/my-page");
+//       } else if (uuid) {
+//         axios
+//           .get(`${process.env.NEXT_PUBLIC_API_URL}/profile/get-profile/${uuid}`)
+//           .then((response) => {
+//             setUser(response.data);
+//           })
+//           .catch((error) => {
+//             console.error("Error fetching user data:", error);
+//           });
+//       }
+//     };
+
+//     checkAndRedirect();
+//   }, [uuid, router]);
